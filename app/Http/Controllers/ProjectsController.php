0a0369b5dacce377d13679ad4df19831313490fa
Teatoller <steven.otieno1@gmail.com>
2019-03-22 09:45:43 +0300

    (controller): implement projects controller
    - create projects controller
    
    - add index function
    
    - return projects view
    
    (#006)

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    public function index() 
+    {
+        $projects = Project::All();
+
+// php interprates this as a way of returning JSON
+        // return $projects;
+        
+        return view('projects.index', ['projects' => $projects]);
+
+        // or
+
+        // return view('projects.index', compact('projects'));
+    }
+}
