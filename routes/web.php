1c5482ef9f12d12322779ba0a492b49b2ed64836
Teatoller <steven.otieno1@gmail.com>
2019-04-19 21:03:55 +0300

    (routes): implement new function route
    - add route to display, edit, update, and delete
    
    [Deliver #27]

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+    GET /projects (index)
+    GET /projects/create (create)
+    GET /projects/1 (show)
+    GET /projects/store (store)
+    GET /projects/1/edit (edit)
+    GET /projects/1 (update)
+    GET /projects/1 (destroy)
+*/
+
 Route::get('/projects', 'ProjectsController@index');
-
-Route::post('/projects', 'ProjectsController@store');
-
 Route::get('/projects/create', 'ProjectsController@create');
+Route::get('/projects/{projects}', 'ProjectsController@show');
+Route::post('/projects', 'ProjectsController@store');
+Route::get('/projects/{projects}/edit', 'ProjectsController@edit');
+Route::patch('/projects/{projects}', 'ProjectsController@update');
+Route::delete('/projects/{projects}', 'ProjectsController@destroy');
