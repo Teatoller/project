de74843c8903e9b4f21a7260c47d2d4104afbfc1
Teatoller <steven.otieno1@gmail.com>
2019-04-20 10:11:33 +0300

    (edit view): implement input field
    - add textarea
    - add update button
    - add css style
    
    [Delivers #014]

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1>Edit Project</h1>
+<h1 class="title">Edit Project</h1>
 
 <form>
     <div class="field">
@@ -10,6 +10,20 @@
 
         <div class="control">
             <input type="text" class="input" name="title" placeholder="Title">
+        </div>
+    </div>
+
+        <div class="field">
+            <label for="description" class="label">Description</label>
+            
+            <div class="control">
+            <textarea name="description" id="" cols="30" rows="10" class="textarea"></textarea>
+            </div>
+        </div>
+
+    <div class="field">
+        <div class="control">
+                <button type="submit" class="button is-link">Update Project</button>
         </div>
     </div>
 </form>
