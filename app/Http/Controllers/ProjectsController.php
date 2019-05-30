e2f61c86b4fbdd1247aaa51a08594351108936f7
Teatoller <steven.otieno1@gmail.com>
2019-05-30 15:03:24 +0300

    (ft-validator): Implement code refactor
    - refactor the validate function in store
    
    [Delivers #32]

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -30,12 +30,12 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        request()->validate([
+        $attributes = request()->validate([
             'title' => ['required','min:3'],
             'description' => ['required', 'min:3']
         ]);
 
-        Project::create(request(['title', 'description']));
+        Project::create($attributes);
 
 
         return redirect('/projects');
