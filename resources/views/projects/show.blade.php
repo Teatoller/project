04e5c930e81bda495b23002b744099c3b8a495ea
Teatoller <steven.otieno1@gmail.com>
2019-08-13 09:21:56 +0300

    (task): implement task checkbox
    - add form with checkbox input field
    - add method and action to update task
    - add onchange function to submit task change
    
    [Starts #037]

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,20 +2,30 @@
 
 @section('content')
 
-    <h1 class="title">{{ $project->title }}</h1>
-    <div class="content">{{ $project->description }}
-        <p>
-            <a href="/projects/{{ $project->id }}/edit">Edit</a>
-        </p>
-    </div>
+<h1 class="title">{{ $project->title }}</h1>
+<div class="content">{{ $project->description }}
+    <p>
+        <a href="/projects/{{ $project->id }}/edit">Edit</a>
+    </p>
+</div>
 
-    @if ($project->tasks->count())
-        <div>
-            @foreach ($project->tasks as $task)
-                <li>{{ $task->description  }}</li>
-            @endforeach
-        </div>
-    @endif
+@if ($project->tasks->count())
+<div>
+    @foreach ($project->tasks as $task)
+    <div>
+        <form method="POST" action="/tasks/{{ $task->id }}" >
+            @method('PATCH')
+            
+            <label class="checkbox" for="completed">
+                <input type="checkbox" name="completed" onchange="this.form.submit()" >
+                {{ $task->description  }}
+            </label>
+        </form>
+
+    </div>
+    @endforeach
+</div>
+@endif
 
 
 @endsection
